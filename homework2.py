c20210c9be80fee891865471c6caa42a83483c45
Евгений Лысенко <udjiedwards@gmail.com>
2025-12-17 01:32:46 +0200

    chage requirements for min,max

--- homework2.py
+++ homework2.py
@@ -3,10 +3,10 @@ import random
 def get_min_max_quantity(): # Функція для отримання даних з користувача та перевірки введених даних
     while True:
         min_value = int(input(f"Enter the minimum from 1 to 1000: ")) # Введення мінімального значення
-        max_value = int(input(f"Enter the maximum from {(min_value+1)} to 1000: ")) # Введення максимального значення
+        max_value = int(input(f"Enter the maximum from {(min_value)} to 1000: ")) # Введення максимального значення
         quantity = int(input(f"Enter the quantity of numbers from 1 to {(max_value - min_value + 1)}: ")) # Введення кількості випадкових чисел
        
-        if 0 < min_value < max_value and max_value < 1001 and 0 < quantity <= (max_value - min_value + 1): # Перевірка коректності введених даних
+        if 0 < min_value <= max_value and max_value < 1001 and 0 < quantity <= (max_value - min_value + 1): # Перевірка коректності введених даних
             return min_value, max_value, quantity
         else:   
             print("Invalid input. Please try again.") # Повідомлення про помилку
