d4a7fbe974ccd2febe8e8387aac361462bdd831b
Евгений Лысенко <udjiedwards@gmail.com>
2025-12-18 01:23:59 +0200

    chage def by task

--- homework1.py
+++ homework1.py
@@ -1,18 +1,15 @@
 # the number of days between a given date and a current date
 from datetime import datetime
-today_date = datetime.now() # Отримання поточнї дату
-def get_valid_date(): # Перевірка введенної дати
-    while True:
-        input_date = input("Enter a date in the format YYYY-MM-DD: ")
-        try:
-            return datetime.strptime(input_date, "%Y-%m-%d") # Перевірка формату дати
-        except ValueError:
-            print("Invalid date format. Please try again.") # Повідомлення про помилку при неправильному форматі дати
 
-input_date = get_valid_date()
+def get_days_from_today(input_date): # Розрахунок кількості днів між двома датами
+    try:
+            input_date = datetime.strptime(input_date, "%Y-%m-%d") # Перетворення дати в об'єкт datetime 
+            today_date = datetime.now() # Отримання поточнї дату
+            return (input_date - today_date).days 
+    except ValueError:
+            return "Invalid date format. Please try again." # Повідомлення про помилку при неправильному форматі дати
 
-def get_days_from_today(date): # Розрахунок кількості днів між двома датами
-    return (today_date - date).days
- 
-print(get_days_from_today(input_date)) # Вивід кількості днів між двома датами
+input_date = input("Enter a date in the format YYYY-MM-DD: ") # Введення дати в форматі YYYY-MM-DD
+
+print(get_days_from_today(input_date))  # Виклик функції для отримання кількості днів між двома датами
 
